42aa1dbf99bb05ee35d18bb55c5f30f1dae8f59c
Soumya Majumder <103351485+isoumya16@users.noreply.github.com>
2024-04-22 08:07:38 +0000

    netflix project

--- react-project-1/awesomeapp/src/index.js
+++ react-project-1/awesomeapp/src/index.js
@@ -1,5 +1,29 @@
+import Card from "./components/Card";
+import Sdata from "./components/Sdata";
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render( <App/>, document.getElementById('root'));
+
+ReactDOM.render(
+
+  <>
+  <h1 className="heading_style"> List Of Top 5 Netflix Series in 2024 </h1>
+  
+  {
+  Sdata.map((val) => <Card
+        imgsrc = {val.imgsrc}
+        seriestitle = {val.seriestitle}
+        seriesname = {val.seriesname}
+        serieslink = {val.serieslink} 
+      />
+  )
+}
+</>
+
+, document.getElementById('root')
+)
+
+
+
+
+  
